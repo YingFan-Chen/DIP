69316f5991542e20cfd55d1baa88dd2e7fe98ac0
YingFan-Chen <b07502024@ntu.edu.tw>
2022-09-28 01:49:48 +0800

    update opencv

--- OpenCV/test.py
+++ OpenCV/test.py
@@ -1,3 +1,12 @@
 import lib
 import cv2
 import numpy as np
+
+img = cv2.imread("tmp.jpg")
+img = cv2.resize(img, (512, 512))
+res = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+res[:,:,2] = cv2.equalizeHist(res[:,:,2])
+res = cv2.cvtColor(res, cv2.COLOR_RGBA2RGB)
+cv2.imshow("res", res)
+cv2.imshow("img", img)
+cv2.waitKey(0)

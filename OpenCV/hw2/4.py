b3feb845c6b5348fd8db4e08e7e7e19315b3163f
YingFan-Chen <b07502024@ntu.edu.tw>
2022-10-25 19:48:17 +0800

    update opencv

--- OpenCV/hw2/4.py
+++ OpenCV/hw2/4.py
@@ -1,10 +1,62 @@
 import cv2
 import math
+from cv2 import GaussianBlur
 import numpy as np
 import matplotlib.pyplot as plt
 plt.xticks([]), plt.yticks([])
 
+def GLPT(P, Q, D):
+    res = np.zeros((P, Q))
+    mid = [P/2, Q/2]
+    for i in range(P):
+        for j in range(Q):
+            dis = math.dist(mid, [i, j])
+            res[i, j] = math.e ** (- dis ** 2 / (2 * D ** 2))
+    return res
+
+def motion_blur(size, angle):
+    res= np.zeros((size, size))
+    res[(size - 1)//2 ,:] = np.ones(size)
+    res = cv2.warpAffine(res, cv2.getRotationMatrix2D((size/2 - 0.5, size/2 - 0.5) , angle, 1.0), (size, size))  
+    res = res / np.sum(res)       
+    return res
+
+def wiener_filter(img, kernal, k):
+    kernal = kernal / np.sum(kernal)
+    dummy = np.copy(img)
+    dummy = np.fft.fft2(dummy)
+    kernal = np.fft.fft2(kernal, s = img.shape)
+    kernal = np.conj(kernal) / (np.abs(kernal) ** 2 + k)
+    dummy = dummy * kernal
+    dummy = np.fft.ifft2(dummy)
+    return dummy.real
+
 img1 = cv2.imread("./images/Photographer_degraded.tif", cv2.IMREAD_GRAYSCALE)
 h, w = img1.shape
-G = np.fft.fft2(img1)
-G = np.fft.fftshift(G)
+print(img1.shape)
+kernal = motion_blur(5, 45)
+NSR = 0.001
+tmp = wiener_filter(img1, kernal, NSR)
+plt.subplot(121)
+plt.imshow(img1, cmap="gray")
+plt.subplot(122)
+tmp = np.fft.fft2(tmp)
+tmp = np.fft.fftshift(tmp)
+G = GLPT(h, w, 10)
+tmp = tmp * G
+tmp = np.fft.fftshift(tmp)
+tmp = np.fft.ifft2(tmp).real
+plt.imshow(tmp, cmap="gray", vmin=0, vmax=255)
+plt.show()
+
+img2 = cv2.imread("./images/Football players_degraded.tif", cv2.IMREAD_GRAYSCALE)
+h, w = img2.shape
+print(img2.shape)
+kernal = motion_blur(10, 40)
+NSR = 0.001
+tmp = wiener_filter(img2, kernal, NSR)
+plt.subplot(121)
+plt.imshow(img2, cmap="gray")
+plt.subplot(122)
+plt.imshow(tmp, cmap="gray", vmin=0, vmax=255)
+plt.show()
--- OpenCV/hw2/test.py
+++ OpenCV/hw2/test.py
@@ -4,102 +4,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.xticks([]), plt.yticks([])
 
-# (u, v) : notch center, (P, Q) : dft.shape, (d, n) : Butterworth parameters
-# lower pass filter
-def Butterworth(u, v, d, n, h, w):
-    def Dk(x, y, u, v):
-        return ((x - u) ** 2 + (y - v) ** 2) ** 0.5
-    res = np.zeros((h, w))
-    for x in range(h):
-        for y in range(w):
-            res[x, y] = (1 / (1 + (Dk(x, y, u, v) / d) ** n)) * (1 / (1 + (Dk(h - x, w - y, u, v) / d) ** n))
+def H(a, b, size, T):
+    res = np.zeros(size, dtype="complex")
+    M, N = size
+    for u in range(M):
+        for v in range(N):
+            Q = math.pi * (u * a + v * b)
+            res[u, v] = math.sin(Q) * math.e ** (complex(0, -Q)) + complex(1e-31, 1e-31)
     return res
 
-def Ideal(u, v, d, h, w):
-    def Dk(x, y, u, v):
-        return ((x - u) ** 2 + (y - v) ** 2) ** 0.5
-    res = np.zeros((h, w))
-    for x in range(h):
-        for y in range(w):
-            if Dk(x, y, u, v) > d:
-                res[x, y] = 0
-            else:
-                res[x, y] = 1
+def motion_blur(size, angle):
+    res= np.zeros((size, size))
+    res[(size - 1)//2 ,:] = np.ones(size)
+    res = cv2.warpAffine(res, cv2.getRotationMatrix2D((size/2 - 0.5 , size/2 - 0.5) , angle, 1.0), (size, size))  
+    res = res / np.sum(res)       
     return res
 
-img = plt.imread("./images/Martian terrain.tif", format="gray")
-print(img.shape)
-plt.imshow(img, cmap = "gray")
+size = (256,256)
+H = H(10, 10, size)
+plt.subplot(121)
+plt.imshow(20*np.log(abs(H)), cmap = "gray")
+m = motion_blur(10, 315)
+M = np.fft.fft2(m, s = size)
+plt.subplot(122)
+plt.imshow(20*np.log(abs(M)), cmap = "gray")
 plt.show()
-
-shift = np.zeros((h, w))
-for x in range(h):
-    for y in range(w):
-        shift[x, y] = img[x, y] # * (-1) ** (x + y)
-dft = cv2.dft(np.float32(shift), flags = cv2.DFT_COMPLEX_OUTPUT)
-
-# show spectrum image(weird that different from textbook)
-mag = 20*np.log(cv2.magnitude(dft[:,:,0], dft[:,:,1]))
-for x in range(h):
-    for y in range(w):
-        if mag[x, y] < 180:
-            mag[x, y] = 0
-plt.imshow(mag, cmap = "gray")
-plt.show()
-'''
-# To Do
-d = 10
-n = 1
-filter = np.ones((h, w))
-filter *= Butterworth(h//2, 0, n, d, h, w)
-filter *= Butterworth(h//2, w, n, d, h, w)
-filter *= Butterworth(0, w//2, n, d, h, w)
-filter *= Butterworth(h, w//2, n, d, h, w)
-
-plt.imshow(filter, cmap = "gray")
-plt.show()
-            
-N = np.zeros((h, w, 2))
-N[:,:,0] = filter * dft[:,:,0]
-N[:,:,1] = filter * dft[:,:,1]
-plt.imshow(20*np.log(cv2.magnitude(N[:,:,0], N[:,:,1])), cmap = "gray")
-plt.show()
-
-eta = cv2.idft(N)[:,:,0]
-for x in range(h):
-    for y in range(w):
-        eta[x, y] = eta[x, y] # * (-1) ** (x + y)
-
-weight = np.zeros((h, w))
-pad_img = np.zeros((h + 4, w + 4))
-pad_img[2:h+2, 2:w+2] = img
-pad_eta = np.zeros((h + 4, w + 4))
-pad_eta[2:h+2, 2:w+2] = eta
-for x in range(h):
-    for y in range(w):
-        t1, t2, t3, b1, b2 = 0, 0, 0, 0, 0
-        for i in range(5):
-            for j in range(5):
-                t1 += pad_img[x + i, y + j] * pad_eta[x + i, y + j]
-                t2 += pad_img[x + i, y + j]
-                t3 += pad_eta[x + i, y + j]
-                b1 += pad_eta[x + i, y + j] * pad_eta[x + i, y + j]
-                b2 += pad_eta[x + i, y + j]
-        t1 /= 25
-        t2 /= 25
-        t3 /= 25
-        b1 /= 25
-        b2 /= 25
-        if b1 - b2 * b2 == 0:
-            weight[x, y] = 1
-        else:
-            weight[x, y] = (t1 - t2 * t3) / (b1 - b2 * b2)
-
-eta *= weight
-plt.imshow(eta, cmap = "gray")
-plt.show()
-
-img = np.float64(img) - eta
-plt.imshow(img, cmap = "gray")
-plt.show()
-'''

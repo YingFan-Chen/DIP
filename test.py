b99adbf40039b3e939ceede0b1312361419f1109
YingFan-Chen <b07502024@ntu.edu.tw>
2023-01-16 12:59:43 +0800

    update test.py

--- test.py
+++ test.py
@@ -1,21 +1,33 @@
-import numpy as np
+import matplotlib.pyplot as plt
+'''
+plt.ylim(80,100)
+x = ['Cifar-100', 'I21K']
+y = [85.55, 98.75]
+plt.plot(x, y, 'o', label = 'ViT-B')
 
-s = np.array([1,3,5,6,6,7,7,7])
-g = np.array([0,0,0,1,2,5,6,7])
-match = np.zeros(8)
-r = 7
-z = 7
-while r >= 0:
-    if s[r] < g[z]:
-        z -= 1
-    elif s[r] == g[z]:
-        if g[z] == g[z - 1]:
-            z -= 1
-        else:
-            match[r] = z
-            r -= 1
-    else:
-        match[r] = z
-        r -= 1
+y = [83.43, 98.95]
+plt.plot(x, y, '*', label = 'ViT-L')
 
-print(match)
+y = [83.67, 97.64]
+plt.plot(x, y, 'x', label = 'ViT-H')
+
+plt.xlabel('Pretrain model')
+plt.ylabel('Accuracy')
+
+plt.legend()
+plt.grid()
+plt.show()
+''' 
+
+import subprocess, os
+from multiprocessing import Process
+
+def foo(str):
+    os.execl('C:\\Program Files\\DAUM\\PotPlayer\\PotPlayerMini64.exe', 'PotPlayerMini64.exe', str)
+
+  
+str = 'F:\\New folder\\1\\ipx-202.mp4'
+p = Process(target=foo, args=(str, ))
+if __name__ == '__main__':
+    p.start()
+    print('Yes')
